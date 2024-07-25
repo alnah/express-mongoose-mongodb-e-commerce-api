c4e1e5148d069fb30d16b99423d9ee06ae5f0d3d
alnah <alexis.nahan@gmail.com>
2024-07-25 14:43:26 -0300

    feat(errors): centralize error classes into an index for exports

---
+++ src/errors/index.js
@@ -0,0 +1,11 @@
+const BadRequestError = require("./badRequestError");
+const CustomApiError = require("./customApiError");
+const NotRouteError = require("./notRouteError");
+const UnauthenticatedError = require("./unauthenticatedError");
+
+module.exports = {
+  BadRequestError,
+  CustomApiError,
+  NotRouteError,
+  UnauthenticatedError,
+};
