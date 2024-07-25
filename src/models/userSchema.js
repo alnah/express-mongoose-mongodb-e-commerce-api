35445484fb5a68eedb7aacba3d5c66f6569cca6b
alnah <alexis.nahan@gmail.com>
2024-07-25 16:34:51 -0300

    feat(models): add an instance method `comparePasswords` in `userSchema`

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -32,4 +32,9 @@ userSchema.pre("save", async function () {
   this.password = hashedPassword;
 });
 
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
+
 module.exports = mongoose.model("User", userSchema);
