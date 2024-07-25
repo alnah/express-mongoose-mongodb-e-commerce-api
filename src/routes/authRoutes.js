feb7a81e29204b836df93352212a37cfa9c8ed93
alnah <alexis.nahan@gmail.com>
2024-07-25 15:35:40 -0300

    feat(auth): add POST routes `/register`, `/login` and `/logout` into an `authRoutes` module

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const { register, login, logout } = require("../controllers/authController");
+
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/logout").post(logout);
+
+module.exports = router;
