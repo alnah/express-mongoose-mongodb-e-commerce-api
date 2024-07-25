a23588fea07dd256ede7007362fd888505a6516d
alnah <alexis.nahan@gmail.com>
2024-07-25 15:25:38 -0300

    feat(models): add an `userSchema`

---
+++ src/models/userSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required. Please enter your name."],
+    minLength: 3,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required. Please enter a valid email address."],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required. Please enter a password."],
+    minLength: 6,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
