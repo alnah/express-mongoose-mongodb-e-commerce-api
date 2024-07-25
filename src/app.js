0c04f049f7fd17ad37d67f1d4c1fb9f5dbc60242
alnah <alexis.nahan@gmail.com>
2024-07-25 15:07:36 -0300

    feat(server): setup an `express` server with a JSON parser, root route, middlewares and `startServer` helper

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
-console.log("npm run dev works!");
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+
+const connectDatabase = require("./database/connectDatabase");
+const {
+  routeNotFoundMiddleware: routeNotFound,
+  errorHandlerMiddleware: errorHandler,
+} = require("./middlewares");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("<h1>Welcome on this eCommerce API!</h1>"));
+
+app.use(routeNotFound);
+app.use(errorHandler);
+
+const startServer = async () => {
+  try {
+    await connectDatabase(process.env.MONGO_URI);
+    console.log("Connected to the database successfully!");
+    app.listen(port, () => console.log(`Server listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
