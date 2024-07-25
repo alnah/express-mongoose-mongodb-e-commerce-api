6c805f6f907d0f67536dc44fdd85e558a67418ef
alnah <alexis.nahan@gmail.com>
2024-07-25 14:55:39 -0300

    feat(errors): centralize error middlewares into an index for exports

---
+++ src/middlewares/index.js
@@ -0,0 +1,4 @@
+const errorHandlerMiddleware = require("./errorHandlerMiddleware");
+const routeNotFoundMiddleware = require("./routeNotFoundMiddleware");
+
+module.exports = { errorHandlerMiddleware, routeNotFoundMiddleware };
