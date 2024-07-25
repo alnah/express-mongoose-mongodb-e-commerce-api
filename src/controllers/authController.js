6f80571d63b0011790f160c6312ec2a7f8a43852
alnah <alexis.nahan@gmail.com>
2024-07-25 16:17:00 -0300

    feat(auth): check if an email already exists in the `register` function of `authController``

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,15 @@
+const { StatusCodes: SC } = require("http-status-codes");
+
+const { userModel } = require("../models");
+const { BadRequestError } = require("../errors");
+
 const register = async (req, res) => {
-  res.send("register user");
+  if (await userModel.findOne({ email: req.body.email })) {
+    throw new BadRequestError("This email already exists.");
+  }
+
+  const user = await userModel.create(req.body);
+  res.status(SC.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
