a9ae2e439726abf66c7e937e86e233f792889c7a
alnah <alexis.nahan@gmail.com>
2024-07-25 18:32:41 -0300

    feat(auth): set `res.cookie` with a token that will be sent to the client in the `register` function of `authController`

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,6 +5,8 @@ const { userModel } = require("../models");
 const { BadRequestError } = require("../errors");
 const { createJwt } = require("../utils");
 
+const ONE_DAY = 1000 * 60 * 60 * 24;
+
 const register = async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -16,7 +18,14 @@ const register = async (req, res) => {
   const tokenUser = { name: user.name, userId: user._id, role: user.role };
   const token = createJwt({ payload: tokenUser });
 
-  res.status(SC.CREATED).json({ user: tokenUser, token });
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "Strict",
+  });
+
+  res.status(SC.CREATED).json({ user: tokenUser });
 };
 
 const login = async (req, res) => {
