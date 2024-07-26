51ec2048112358978e70b53cbf3f7178efa1c9e2
alnah <alexis.nahan@gmail.com>
2024-07-25 21:16:46 -0300

    feat(server): use JSON Web Token secret into `cookieParser` to access `req.signedCookies`

--- src/app.js
+++ src/app.js
@@ -15,14 +15,22 @@ const port = process.env.PORT || 3000;
 
 app.use(morgan("tiny"));
 app.use(express.json());
+<<<<<<< HEAD
+=======
+app.use(cookieParser(process.env.JWT_SECRET));
+>>>>>>> 76402d6 (feat(server): use JSON Web Token secret into `cookieParser` to access `req.signedCookies`)
 
 <<<<<<< HEAD
 app.get("/", (req, res) => res.send("<h1>Welcome on this eCommerce API!</h1>"));
 =======
 // routes
 app.get("/", (req, res) => res.send("<h1>Welcome on this eCommerce API!</h1>"));
+<<<<<<< HEAD
 app.get("/api/v1", (req, res) => console.log(req.cookies));
 >>>>>>> 21388eb (feat(server): setup `cookieParser` and log `req.cookies` in the GET route `/api/v1` for testing purposes)
+=======
+app.get("/api/v1", (req, res) => console.log(req.signedCookies));
+>>>>>>> 76402d6 (feat(server): use JSON Web Token secret into `cookieParser` to access `req.signedCookies`)
 app.use("/api/v1/auth", authRoutes);
 
 app.use(routeNotFound);
