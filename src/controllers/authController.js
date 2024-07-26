09e011776648e9475a33bb24ad97ab4d922e895b
alnah <alexis.nahan@gmail.com>
2024-07-25 21:32:20 -0300

    feat(auth): add `login` function in the `authController`

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,22 +1,35 @@
 const { userModel } = require("../models");
-const { BadRequestError } = require("../errors");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 const { attachCookiesToResponse } = require("../utils");
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
-
   if (await userModel.findOne({ email })) {
     throw new BadRequestError("This email already exists.");
   }
-
   const user = await userModel.create({ name, email, password });
   const tokenUser = { name: user.name, userId: user._id, role: user.role };
-
   attachCookiesToResponse({ res, user: tokenUser });
 };
 
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+  if (!email) {
+    throw new BadRequestError("Please provide an email.");
+  }
+  if (!password) {
+    throw new BadRequestError("Please provide a password.");
+  }
+  const user = await userModel.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid credentials.");
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid credentials.");
+  }
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  attachCookiesToResponse({ res, user: tokenUser });
 };
 
 const logout = async (req, res) => {
