512fe41b00fad85626439db6f663316c91111376
alnah <alexis.nahan@gmail.com>
2024-07-26 20:09:47 -0300

    feat(models): add `reviewModel` into the index for exports

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,5 @@
 const userModel = require("./userModel");
 const productModel = require("./productModel");
+const reviewModel = require("./reviewModel");
 
-module.exports = { userModel, productModel };
+module.exports = { userModel, productModel, reviewModel };
