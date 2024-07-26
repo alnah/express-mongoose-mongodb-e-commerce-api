ad0dd22bcb8fa49d74b694b627477779669fafc8
alnah <alexis.nahan@gmail.com>
2024-07-26 20:50:37 -0300

    feat(review): add `getAllReviews` logic of the `reviewController`

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -4,7 +4,8 @@ const { NotFoundError, BadRequestError } = require("../errors");
 const { reviewModel, productModel } = require("../models");
 
 const getAllReviews = async (req, res, next) => {
-  res.send("get all reviews");
+  const reviews = await reviewModel.find({});
+  res.status(SC.OK).json({ reviews, count: reviews.length });
 };
 
 const createReview = async (req, res, next) => {
