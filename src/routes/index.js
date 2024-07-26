9b9135b98406fbd6513b3f21ead49da71299fe8e
alnah <alexis.nahan@gmail.com>
2024-07-26 20:21:53 -0300

    feat(review): add a `reviewRoutes` module and export it via routes index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const authRoutes = require("./authRoutes");
 const userRoutes = require("./userRoutes");
 const productRoutes = require("./productRoutes");
+const reviewRoutes = require("./reviewRoutes");
 
-module.exports = { authRoutes, userRoutes, productRoutes };
+module.exports = { authRoutes, userRoutes, productRoutes, reviewRoutes };
