66ae3649372c76ef1c739fef051f06d5a1222538
alnah <alexis.nahan@gmail.com>
2024-07-25 21:51:44 -0300

    fix(server): rewrite `app.js` with the correct logic due to unsresolved merge conflicts

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 
 const { authRoutes } = require("./routes");
 const {
@@ -13,29 +14,23 @@ const connectDatabase = require("./database/connectDatabase");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// logger
 app.use(morgan("tiny"));
-app.use(express.json());
-<<<<<<< HEAD
-=======
-app.use(cookieParser(process.env.JWT_SECRET));
->>>>>>> 76402d6 (feat(server): use JSON Web Token secret into `cookieParser` to access `req.signedCookies`)
 
-<<<<<<< HEAD
-app.get("/", (req, res) => res.send("<h1>Welcome on this eCommerce API!</h1>"));
-=======
+// parsers
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+
 // routes
 app.get("/", (req, res) => res.send("<h1>Welcome on this eCommerce API!</h1>"));
-<<<<<<< HEAD
-app.get("/api/v1", (req, res) => console.log(req.cookies));
->>>>>>> 21388eb (feat(server): setup `cookieParser` and log `req.cookies` in the GET route `/api/v1` for testing purposes)
-=======
 app.get("/api/v1", (req, res) => console.log(req.signedCookies));
->>>>>>> 76402d6 (feat(server): use JSON Web Token secret into `cookieParser` to access `req.signedCookies`)
 app.use("/api/v1/auth", authRoutes);
 
+// errors
 app.use(routeNotFound);
 app.use(errorHandler);
 
+// helper
 const startServer = async () => {
   try {
     await connectDatabase(process.env.MONGO_URI);
@@ -46,4 +41,5 @@ const startServer = async () => {
   }
 };
 
+// start
 startServer();
