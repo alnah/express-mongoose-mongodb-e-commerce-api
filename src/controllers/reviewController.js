4ebe162ab89c1aece655d0bd30f4e9129d10a7b3
alnah <alexis.nahan@gmail.com>
2024-07-26 20:14:49 -0300

    feat(review): add a `reviewController` module with placeholders function

---
+++ src/controllers/reviewController.js
@@ -0,0 +1,27 @@
+const getAllReview = async (req, res, next) => {
+  res.send("get all reviews");
+};
+
+const createReview = async (req, res, next) => {
+  res.send("create a review");
+};
+
+const getSingleReview = async (req, res, next) => {
+  res.send("get a single review");
+};
+
+const updateReview = async (req, res, next) => {
+  res.send("update a review");
+};
+
+const deleteReview = async (req, res, next) => {
+  res.send("delete a review");
+};
+
+module.exports = {
+  getAllReview,
+  createReview,
+  getSingleReview,
+  updateReview,
+  deleteReview,
+};
