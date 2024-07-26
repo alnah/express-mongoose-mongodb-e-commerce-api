3a7e6ac4e9e8726b43f4489b737cc260cd40a109
alnah <alexis.nahan@gmail.com>
2024-07-26 13:26:06 -0300

    feat(errors): add an `UnauthorizedError` class and export it via errors' index

--- src/errors/index.js
+++ src/errors/index.js
@@ -2,10 +2,12 @@ const BadRequestError = require("./badRequestError");
 const CustomApiError = require("./customApiError");
 const NotRouteError = require("./notRouteError");
 const UnauthenticatedError = require("./unauthenticatedError");
+const UnauthorizedError = require("./unauthorizedError");
 
 module.exports = {
   BadRequestError,
   CustomApiError,
   NotRouteError,
   UnauthenticatedError,
+  UnauthorizedError,
 };
