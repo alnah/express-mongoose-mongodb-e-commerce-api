daa92f1ff122adbb094552d952e1c5ad032b5eac
alnah <alexis.nahan@gmail.com>
2024-07-25 21:10:08 -0300

    refactor(auth): refactor `register` function of the `authController` with `attachCookiesToReponse` helper

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,6 @@
-const { StatusCodes: SC } = require("http-status-codes");
-const jwt = require("jsonwebtoken");
-
 const { userModel } = require("../models");
 const { BadRequestError } = require("../errors");
-const { createJwt } = require("../utils");
-
-const ONE_DAY = 1000 * 60 * 60 * 24;
+const { attachCookiesToResponse } = require("../utils");
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -16,16 +11,8 @@ const register = async (req, res) => {
 
   const user = await userModel.create({ name, email, password });
   const tokenUser = { name: user.name, userId: user._id, role: user.role };
-  const token = createJwt({ payload: tokenUser });
 
-  res.cookie("token", token, {
-    httpOnly: true,
-    expires: new Date(Date.now() + ONE_DAY),
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
-  });
-
-  res.status(SC.CREATED).json({ user: tokenUser });
+  attachCookiesToResponse({ res, user: tokenUser });
 };
 
 const login = async (req, res) => {
