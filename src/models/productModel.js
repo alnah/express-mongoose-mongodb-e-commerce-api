a7ffb0b9a114cca7489b40eff054404689941ed9
alnah <alexis.nahan@gmail.com>
2024-07-27 13:38:46 -0300

    fix(product): use `deleteOne` instead of `remove` in the pre-hook, and clarify that it is the document method, not the query method, that we are working with

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -90,8 +90,12 @@ productSchema.virtual("reviews", {
   justOne: false,
 });
 
-productSchema.pre("remove", async function (next) {
-  await this.model("Review").deleteMany({ product: this._id });
-});
+productSchema.pre(
+  "deleteOne",
+  { document: true, query: false },
+  async function () {
+    await this.model("Review").deleteMany({ product: this._id });
+  }
+);
 
 module.exports = mongoose.model("Product", productSchema);
