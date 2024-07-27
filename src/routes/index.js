5cae277292e8857c32f48106ca9145db3164c6dd
alnah <alexis.nahan@gmail.com>
2024-07-27 15:44:43 -0300

    feat(order): add `userRoutes` to the index routes for exports

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,5 +2,12 @@ const authRoutes = require("./authRoutes");
 const userRoutes = require("./userRoutes");
 const productRoutes = require("./productRoutes");
 const reviewRoutes = require("./reviewRoutes");
+const orderRoutes = require("./orderRoutes");
 
-module.exports = { authRoutes, userRoutes, productRoutes, reviewRoutes };
+module.exports = {
+  authRoutes,
+  userRoutes,
+  productRoutes,
+  reviewRoutes,
+  orderRoutes,
+};
